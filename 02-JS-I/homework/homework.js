3ebae53783b505d3e980ace00751828b2a6769ae
lucascalvino <lucascalvino90@gmail.com>
2021-02-19 18:26:56 -0300

    CP commiteado por lucascalvino1 a las 1613770016033

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -225,7 +225,7 @@ function esVocal(letra){
 }
 
 // No modificar nada debajo de esta línea
-// --------------------------------
+// -------------------------------
 
 module.exports = {
   nuevaString,
