f4acfc12029f3c9ab0375ea82eb3342927704405
lucascalvino <lucascalvino90@gmail.com>
2021-02-21 16:32:06 -0300

    finalizado js 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,21 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  keys=Object.keys(objeto);
+  values=Object.values(objeto);
+
+  len = keys.length;
+  array=[]
+  for (i=0;i<len;i++){
+    arr2=[]
+    arr2.push(keys[i])
+    arr2.push(values[i])
+    array.push(arr2)
+  }
+  
+
+  return array
+
 }
 
 
@@ -18,6 +33,30 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  dicc={}
+  chars=[]
+  //(string.match(/o/g)||[]).length// "this is foso bar".split("s").length-1
+  for (i=0;i<string.length;i++){
+    char = string[i];
+    count=0;
+    if (char!=" "){
+    
+    if (chars.includes(char)){
+      continue
+    } else {
+
+      count = (string.split(char).length-1);
+      dicc[char]=count
+
+    }
+    
+    chars.push(char)
+
+    
+
+
+  }}
+  return dicc
 }
 
 
@@ -26,8 +65,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  minus = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  mayus=""
+  minusc=""
+  for (i=0;i<s.length;i++){
+  char= s[i]
+  if(minus.includes(char)){
+    minusc=minusc+char
+  }else{mayus=mayus+char}
 }
-
+return mayus+minusc
+}
 
 function asAmirror(str) {
   //La función recibe una frase. 
@@ -35,6 +83,21 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  array_strings = str.split(" ")
+  str_fin=""
+  pala2=""
+  for (i=0;i<array_strings.length;i++){
+    pala = array_strings[i];
+    pala2="";
+    for (j=pala.length-1;j>-1;j=j-1){
+      char2=pala[j];
+      pala2=pala2+char2;
+    }
+    if (i!==(array_strings.length-1)){
+    str_fin=str_fin+pala2+" ";
+    }else{str_fin=str_fin+pala2}
+  }
+  return str_fin
 } 
 
 
@@ -43,6 +106,15 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var capi=""
+  var numero_string = numero.toString();
+  for (j=numero_string.length-1;j>-1;j=j-1){
+    num=numero_string[j];
+    capi=capi+num;
+  }
+
+  if (numero==capi){return "Es capicua"}else{return "No es capicua"}
+
 }
 
 
@@ -50,6 +122,17 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var letras_no = ["a","A","b","B","c","C"]
+  var cadena_def=""
+  for (i=0;i<cadena.length;i++){
+    if(letras_no.includes(cadena[i])){
+      continue
+    }else{cadena_def=cadena_def+cadena[i]
+
+    }
+
+  }
+  return cadena_def
 }
 
 
@@ -57,6 +140,11 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  arr.sort(function(a,b){
+    return a.length - b.length
+  ;})
+  return arr
+
 }
 
 
@@ -65,7 +153,30 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  
+  intersec =[]
+
+  for (i=0;i<arreglo1.length;i++){
+    if (arreglo2.includes(arreglo1[i])){
+      intersec.push(arreglo1[i])
+    }
+  }
+
+  for (i=0;i<arreglo2.length;i++){
+    if (arreglo1.includes(arreglo2[i])){
+      intersec.push(arreglo2[i])
+    }
+  }
+
+  intersec_final=[]
+  for (i=0;i<intersec.length;i++){
+    if (intersec_final.includes(intersec[i])){
+      continue
+    } else { intersec_final.push(intersec[i])}
+  }
+
+  return intersec_final
 }
 
 
